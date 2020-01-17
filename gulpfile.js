870da0985fa02258a32d24ea5a32c9b660ba40ae
victor <victorpaul.war@gmail.com>
2020-01-17 18:01:20 +0200

    little update

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const connect = require('gulp-connect');
 
 const folders = [
     'css', 'icon-fonts', 'img', 'img/icon', 'js', 'skills',
-    'beerme','thoughts', 'thoughts/css', 'thoughts/js', 'thoughts/js/bootstrap', 'thoughts/js/jquery', 'thoughts/js/plugins', 'thoughts/fonts', 'thoughts/img/bg-img', 'thoughts/img/core-img'
+    'beerme', 'thoughts', 'thoughts/css', 'thoughts/js', 'thoughts/js/bootstrap', 'thoughts/js/jquery', 'thoughts/js/plugins', 'thoughts/fonts', 'thoughts/img/bg-img', 'thoughts/img/core-img'
 ];
 
-
-gulp.task('copy-all', function () {
+gulp.task('copy-all', function (done) {
     for (var key in folders)
         gulp.src('www/' + folders[key] + '/*.*').pipe(gulp.dest('dist/' + folders[key])).pipe(connect.reload());
     gulp.src('www/*.*').pipe(gulp.dest('dist/')).pipe(connect.reload());
+    done();
 });
 
 gulp.task('run-server-www', function () {
@@ -27,11 +27,15 @@ gulp.task('watch', function () {
     gulp.watch(['www/*.*'], ['copy-all']);
 });
 
-gulp.task('local-conf', () => {
+gulp.task('local-conf', (done) => {
     gulp.src("conf-dev.js")
         .pipe(rename('config.js'))
         .pipe(gulp.dest('dist/js'))
+    done();
 });
 
-gulp.task('build-loc', ['local-conf', 'copy-all']);
-gulp.task('run-loc', ['build-loc', 'run-server-www', 'watch']);
+gulp.task('build-loc', gulp.series(['local-conf', 'copy-all']), () => {
+
+});
+gulp.task('run-loc', gulp.series(['build-loc', 'run-server-www', 'watch']), () => {
+});
