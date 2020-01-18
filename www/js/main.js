ca2a93ceabee8d5992a3b6d679e83e413c132c52
victorZ <victorpaul.war@gmail.com>
2020-01-18 14:50:26 +0200

    fixy pixy

--- www/js/main.js
+++ www/js/main.js
@@ -6,39 +6,38 @@
  ====================================*/
 
 
-
 'use strict';
 
 
-$(window).on('load', function() { 
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
+$(window).on('load', function () {
+    /*------------------
+        Preloder
+    --------------------*/
+    $(".loader").fadeOut();
+    $("#preloder").delay(400).fadeOut("slow");
 
 });
 
 
-(function($) {
+(function ($) {
 
-	/*------------------
-		Background set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
+    /*------------------
+        Background set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
 
 
-	$('.rs1').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		items: 1,
+    $('.rs1').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        items: 1,
         autoplayTimeout: 10000,
-		autoplay: true
-	});
+        autoplay: true
+    });
 
     $('.rs2').owlCarousel({
         loop: true,
@@ -50,101 +49,102 @@ $(window).on('load', function() {
     });
 
 
-
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
-	});
+    $('.progress-bar-style').each(function () {
+        var progress = $(this).data("progress");
+        var prog_width = progress + '%';
+        if (progress <= 100) {
+            $(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
+        } else {
+            $(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
+        }
+    });
 
 
-	$('.lan-prog').each(function() {
-		var progress = $(this).data("lanprogesss");
-		var ele      = '<span></span>';
-		var ele_fade = '<span class="fade-ele"></span>';
-		
-		for (var i = 1; i <= 5; i++) {
-			if(i <= progress){
-				$(this).append(ele);
-			} else {
-				$(this).append(ele_fade);
-			}
-		}
-	});
+    $('.lan-prog').each(function () {
+        var progress = $(this).data("lanprogesss");
+        var ele = '<span></span>';
+        var ele_fade = '<span class="fade-ele"></span>';
+
+        for (var i = 1; i <= 5; i++) {
+            if (i <= progress) {
+                $(this).append(ele);
+            } else {
+                $(this).append(ele_fade);
+            }
+        }
+    });
 
 
-	/*------------------
-		Popup
-	--------------------*/
-	$('.portfolio-item .port-pic').magnificPopup({
-		type: 'image',
-		mainClass: 'img-popup-warp',
-		removalDelay: 500,
-	});
+    /*------------------
+        Popup
+    --------------------*/
+    $('.portfolio-item .port-pic').magnificPopup({
+        type: 'image',
+        mainClass: 'img-popup-warp',
+        removalDelay: 500,
+    });
 
 
+    if ($().circleProgress) {
 
+        //Set progress circle 1
+        $("#progress1").circleProgress({
+            value: 0.75,
+            size: 175,
+            thickness: 2,
+            fill: "#40424a",
+            emptyFill: "rgba(0, 0, 0, 0)"
+        });
+        //Set progress circle 2
+        $("#progress2").circleProgress({
+            value: 0.83,
+            size: 175,
+            thickness: 2,
+            fill: "#40424a",
+            emptyFill: "rgba(0, 0, 0, 0)"
+        });
 
-if($().circleProgress){
+        //Set progress circle white
+        $("#progress3").circleProgress({
+            value: 0.75,
+            size: 175,
+            thickness: 2,
+            fill: "#ffffff",
+            emptyFill: "rgba(0, 0, 0, 0)"
+        });
 
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
+        //Set progress circle white
+        $("#progress4").circleProgress({
+            value: 0.83,
+            size: 175,
+            thickness: 2,
+            fill: "#ffffff",
+            emptyFill: "rgba(0, 0, 0, 0)"
+        });
 
-	//Set progress circle white
-	$("#progress3").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
+        //Set progress circle skyblue
+        $("#progress5").circleProgress({
+            value: 0.75,
+            size: 175,
+            thickness: 2,
+            fill: "#009fff",
+            emptyFill: "rgba(0, 0, 0, 0)"
+        });
 
-	//Set progress circle white
-	$("#progress4").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
+        //Set progress circle skyblue
+        $("#progress6").circleProgress({
+            value: 0.83,
+            size: 175,
+            thickness: 2,
+            fill: "#009fff",
+            emptyFill: "rgba(0, 0, 0, 0)"
+        });
 
-	//Set progress circle skyblue
-	$("#progress5").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle skyblue
-	$("#progress6").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-}
+        $.get('https://api.github.com/repos/victorpaul/web-kosh/commits', (data) => {
+            var commit = data[0];
+            $("#commit").html("Last commit " + commit.sha + " " + commit.commit.author.date);
+        });
+    }
 
 })(jQuery);
 
